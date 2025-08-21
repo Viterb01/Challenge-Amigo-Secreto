a3143152378fc19aa1a6118cb6b2d17abf9b3fee
Gabriel Viterbo <gabrielviterbodeoliveira@hotmail.com>
2025-08-21 07:14:45 -0300

    função adicionarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos=[];
+
+function adicionarAmigo() {
+    nome=document.querySelector('input');
+
+    if (nome.value=='') {
+        alert('Por favor, ensira um nome');
+    } else {
+        amigos.push(nome)
+        nome.value=''
+    }
+}
+
