3b69ac0642252bb09dd4abbfb288966fbe5552e4
Gabriel Viterbo <gabrielviterbodeoliveira@hotmail.com>
2025-08-21 07:36:34 -0300

    Função que lista os amigos na tela

--- app.js
+++ app.js
@@ -3,13 +3,22 @@
 let amigos=[];
 
 function adicionarAmigo() {
-    nome=document.querySelector('input');
+    nome=document.querySelector('input').value;
 
-    if (nome.value=='') {
+    if (nome=='') {
         alert('Por favor, ensira um nome');
     } else {
-        amigos.push(nome)
-        nome.value=''
+        amigos.push(nome);
+        nome.value='';
+        listaAmigos()
     }
 }
 
+function listaAmigos() {
+    lista=document.getElementById('listaAmigos');
+    lista.innerHTML='';
+
+    for (let amigo of amigos) {
+        lista.innerHTML+=`<li>${amigo}<li>`
+    }
+}
